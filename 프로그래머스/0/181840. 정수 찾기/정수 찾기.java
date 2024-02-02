893a7bc2a62f821498d52bfb73822e5344c6823c
bellbu <100392942+bellbu@users.noreply.github.com>
2024-02-02 15:36:53 +0900

    Update 정수 찾기.java

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,14 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] num_list, int n) {
-        int answer = 0;
-        for(int i = 0; i < num_list.length; i++){
-            if (num_list[i] == n) {
-                answer = 1;     
-                break; 
-            } else {
-                answer = 0;
-            }
-        }
-        return answer;
+        return IntStream.of(num_list).anyMatch(num -> num == n) ? 1 : 0;
     }
 }
