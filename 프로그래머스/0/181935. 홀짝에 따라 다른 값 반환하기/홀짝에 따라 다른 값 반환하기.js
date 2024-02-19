0dbb356db6aed27465143af536f3c7a86ab72617
bellbu <100392942+bellbu@users.noreply.github.com>
2024-02-19 20:43:38 +0900

    Update 홀짝에 따라 다른 값 반환하기.js

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,17 +1,6 @@
 function solution(n) {
-    let answer = 0;
-    if(n%2 === 0) {
-        
-        for(let i = 2; i <= n; i+=2) {
-            answer += i*i;                        
-        }
-
-    } else {
-        
-        for(let i = 1; i <= n; i+=2) {
-            answer += i;                        
-        }
-        
-    }
-    return answer;
+    if(n%2===1)
+      return  (n+1)/2*((n + 1)/2) ;
+    else
+      return   n*(n+1)*(n+2)/6;
 }
