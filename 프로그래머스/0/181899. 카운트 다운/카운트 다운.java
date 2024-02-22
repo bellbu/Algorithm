a7222793fe0b1e9fb64e1506cdddbc4135ce8e2d
bellbu <100392942+bellbu@users.noreply.github.com>
2024-02-22 16:29:28 +0900

    Update 카운트 다운.java

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,9 +1,7 @@
+import java.util.stream.*;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[start - end + 1];
-        for(int i = start; i >= end; i--){
-            answer[start-i] = i;
-        }
-        return answer;
+        return IntStream.rangeClosed(-start, -end).map(i -> -i).toArray();
     }
 }
