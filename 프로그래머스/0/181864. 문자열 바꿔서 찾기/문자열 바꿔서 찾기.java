b752f381c872ca6267d4fab6e48ca1017af38871
bellbu <100392942+bellbu@users.noreply.github.com>
2024-02-26 15:02:32 +0900

    Update 문자열 바꿔서 찾기.java

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String str = myString.replace("A","X").replace("B","A").replace("X","B");
+        String str = myString.replace("A","a").replace("B","A").replace("a","B");
         return str.contains(pat) ? 1 : 0;
         
     }
