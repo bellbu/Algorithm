b7eb331a2c2b4a4ba4345913174215d3ec455a58
bellbu <100392942+bellbu@users.noreply.github.com>
2024-03-12 11:52:53 +0900

    Update 꼬리 문자열.java

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,11 +1,8 @@
 class Solution {
     public String solution(String[] str_list, String ex) {
         String answer = "";
-        // 문자열 검색 ex 포함도는지 포함되면 제외 포함안되면 합치기
         for(int i = 0; i < str_list.length; i++){
-            if(str_list[i].contains(ex)){
-                
-            }else{
+            if(!str_list[i].contains(ex)){
                 answer += str_list[i];
             }
         }
