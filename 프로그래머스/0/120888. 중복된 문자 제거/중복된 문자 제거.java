56ad50ac909e08c972c02b063877bef388ed654a
bellbu <100392942+bellbu@users.noreply.github.com>
2025-05-29 19:00:27 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.19 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i < my_string.length(); i++) {
+            char ch = my_string.charAt(i);
+            
+            if(sb.indexOf(String.valueOf(ch)) == -1) {
+                sb.append(ch);
+            }
+            
+        }
+        
+        return sb.toString();
+    }
+}
