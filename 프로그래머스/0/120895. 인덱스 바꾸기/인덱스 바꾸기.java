788d344c2336e75dcf65833b8dd9f71c027484ca
bellbu <100392942+bellbu@users.noreply.github.com>
2025-09-04 00:15:27 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char str1 = my_string.charAt(num1);
+        char str2 = my_string.charAt(num2);
+        
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.setCharAt(num1, str2);
+        sb.setCharAt(num2, str1);
+        return sb.toString();
+    }
+}
